c4d891c822455fbade00ba619e44985fe6bd55f8
Anton Ivanov <a.ivanov@magnus-tech.ru>
2022-11-28 12:55:11 +0300

    :bug: fixed entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Applying migrations..."
 alembic upgrade head
 echo "Migrations applied!"
 
-#echo "Starting the application..."
-#uvicorn application.web.main:app --reload --host 0.0.0.0
+echo "Starting the application..."
+uvicorn application.web.main:app --reload --host 0.0.0.0
 
 exec "$@"
